44bab374a23f192c6a4545129508ae4c434c68f1
Степан Молчанов <stepanmol4anoff@yandex.ru>
2024-02-20 15:07:59 +0300

    Add react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.js';
 import './index.css';
 
+const paths = [
+    {
+        path: '/',
+        element: <App/>,
+    },
+]
+const router = createBrowserRouter(paths);
 
-const root = document.getElementById('root');
-ReactDOM.render(<App/>, root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode>
+    <RouterProvider router={router}/>
+</React.StrictMode>
+);
