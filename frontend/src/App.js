e1139c08d94412f9c9874b05eaa98a94e452d6af
Степан Молчанов <stepanmol4anoff@yandex.ru>
2024-02-20 14:58:58 +0300

    Set up simple frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+
+function App()
+{
+  return (
+      <div className="App">
+        Тело компонента root
+      </div>
+  );
+}
+
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import './index.css';
+
+
+const root = document.getElementById('root');
+ReactDOM.render(<App/>, root);
